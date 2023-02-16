19fcaa8cb7144b903ff68283891162a1fa16e191
KATIA SACO <123751067+Katiasaco@users.noreply.github.com>
2023-02-16 20:53:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -155,14 +155,9 @@ with tab_plots:
         
     import plotly.express as px
     st.write('Vamos a realizar un estudio de los Quakers dependiendo del grano de café:')
+    graf3=px.box(new_df,x= "Species", y = "Quakers",color ="Species",points='all', template="plotly_dark")
+    st.plotly_chart(graf3)
 
-    col1, col2 = st.columns(2)
-    with col1:
-        graf3=px.box(new_df,x= "Species", y = "Quakers",color ="Species",points='all', template="plotly_dark")
-        st.plotly_chart(graf3)
-
-    with col2:
-        st.image('k.png',width= 200)
 
     st.write('QUAKERS POR PAÍS')
     graf4=px.box(new_df,x= "Country.of.Origin", y = "Quakers",color ="Country.of.Origin",points='all', template="plotly_dark")
